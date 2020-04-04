83a57619feb108d1c51599cf258c9c76f857780b
Keidai Izumi <keidai.izumi@gmail.com>
2020-04-04 16:36:36 +0900

    新規関数の追加

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
+/**
+ * 17で割り切れる数を判定する
+ */
+function isMultipleOfSeventeen(n){
+    return n % 17 === 0 ;
+}
+
 module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
 }
